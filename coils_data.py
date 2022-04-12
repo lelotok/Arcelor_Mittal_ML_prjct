1a69debebd2ac4c8fd31c2b695824e1db1e83f5d
lelotok <98822210+lelotok@users.noreply.github.com>
2022-04-12 16:27:47 +0200

    coils_data & data_exploration files

---
+++ coils_data.py
@@ -0,0 +1,44 @@
+import re
+import os
+
+## Capturing all the coil files, path is a relative path from the project, so
+## should be edited to fit your project
+coils = os.listdir("../SignalExport")
+
+for coil in coils:
+
+    with open("../SignalExport/" + str(coil), "r") as read_file:
+
+        string = read_file.read()
+
+    ## This regex will extract the data from the files in 2 capture groups
+    captures = re.search(r'.*Lengthpoints:;(.*)Values;(.*)', string)
+
+    ## If nothing gets captured, the file will be skipped
+    if captures != None:
+        lengthpoints = captures.group(1)
+        values = captures.group(2)
+
+        lengthpoints = lengthpoints.split(";")
+        values = values.split(";")
+
+        ## This both cleans the data and puts it in an iterable array
+        clean_data = [(lengthpoint, value) for (lengthpoint, value) in zip(lengthpoints, values) if lengthpoint != '0' and value != '0']
+
+        ## This is once again a relative path and needs to be edited to point
+        ## to the correct directory
+        with open("../new_coils_data/" + str(coil), "w") as write_file:
+
+            ## Now we write the data to new csv and delete the old file, this
+            ## will lead to us having all cleaned and correct files in the
+            ## directory and leave any faulty files in the old directory, we can
+            ## now iterate over those files and try to remove them if necessary
+            write_file.write("Lengthpoints,Values\n")
+            write_file.writelines(lengthpoint + "," + value + "\n" for (lengthpoint, value) in clean_data)
+
+            os.remove("../SignalExport/" + str(coil))
+
+    else:
+
+        print(coil)
+
