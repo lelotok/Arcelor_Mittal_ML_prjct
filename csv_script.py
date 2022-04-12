fa029b7b93f29e24f9b9ab1969d4542a2951b233
BertramDHooge <bertramdhooge@gmail.com>
2022-04-12 14:35:34 +0200

    finished and commented the script

--- csv_script.py
+++ csv_script.py
@@ -1,25 +1,44 @@
 import re
 import os
 
-directory_list = os.listdir("SignalExport")
-for directory in directory_list:
-    print(directory)
+## Capturing all the coil files, path is a relative path from the project, so
+## should be edited to fit your project
+coils = os.listdir("../Metal_stuff/SignalExport")
 
-    file = open("SignalExport/" + directory, "r")
-    string = file.read()
+for coil in coils:
 
+    with open("../Metal_stuff/SignalExport/" + str(coil), "r") as read_file:
+
+        string = read_file.read()
+
+    ## This regex will extract the data from the files in 2 capture groups
     captures = re.search(r'.*Lengthpoints:;(.*)Values;(.*)', string)
 
-    lengthpoints = captures.group(1)
-    values = captures.group(2)
+    ## If nothing gets captured, the file will be skipped
+    if captures != None:
+        lengthpoints = captures.group(1)
+        values = captures.group(2)
 
-    lengthpoints = lengthpoints.split(";")
-    values = values.split(";")
+        lengthpoints = lengthpoints.split(";")
+        values = values.split(";")
 
-    file.close()
+        ## This both cleans the data and puts it in an iterable array 
+        clean_data = [(lengthpoint, value) for (lengthpoint, value) in zip(lengthpoints, values) if lengthpoint != '0' and value != '0']
 
-    file = open("SignalExport/" + directory, "w")
+        ## This is once again a relative path and needs to be edited to point
+        ## to the correct directory
+        with open("../Metal_stuff/coil_data/" + str(coil), "w") as write_file:
 
-    file.write("Lengthpoints;Values\n")
-    file.writelines(lengthpoint + ";" + value + "\n" for (lengthpoint, value) in zip(lengthpoints, values))
-    file.close()
+            ## Now we write the data to new csv and delete the old file, this
+            ## will lead to us having all cleaned and correct files in the
+            ## directory and leave any faulty files in the old directory, we can
+            ## now iterate over those files and try to remove them if necessary
+            write_file.write("Lengthpoints;Values\n")
+            write_file.writelines(lengthpoint + ";" + value + "\n" for (lengthpoint, value) in clean_data)
+
+            os.remove("../Metal_stuff/SignalExport/" + str(coil))
+
+    else:
+
+        print(coil)
+
