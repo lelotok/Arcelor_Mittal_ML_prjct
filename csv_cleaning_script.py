7c38baa9455edf4d25b48b8250ca3f12e3eb92d7
BertramDHooge <bertramdhooge@gmail.com>
2022-04-14 14:06:30 +0200

    Made paths values at start of file, change them according to your filesystem, and -structure

--- csv_cleaning_script.py
+++ csv_cleaning_script.py
@@ -3,7 +3,11 @@ import os
 import pandas as pd
 import numpy as np
 
-with open("../Metal_stuff/CoilData.csv") as coil_csv:
+PATH_TO_OLD = "../Metal_stuff/SignalExport/"
+PATH_TO_NEW = "../Metal_stuff/coil_data/"
+PATH_TO_CSV = "../Metal_stuff/CoilData.csv"
+
+with open(PATH_TO_CSV) as coil_csv:
     coil_df = pd.read_csv(coil_csv)
 
 bad_coils = []
@@ -11,7 +15,7 @@ bad_coils = []
 for coil in coil_df.coil:
     for index in ["4", "5"]:
         try:
-            with open(f"../Metal_stuff/SignalExport/{coil}B{index}.csv") as read_file:
+            with open(f"{PATH_TO_OLD}{coil}B{index}.csv") as read_file:
                 file_content = read_file.read()
 
             ## This regex will extract the data from the files in 2 capture groups
@@ -34,7 +38,7 @@ for coil in coil_df.coil:
 
                 ## This is once again a relative path and needs to be edited to point
                 ## to the correct directory
-                with open(f"../Metal_stuff/coil_data/{coil}B{index}.csv", "w") as write_file:
+                with open(f"{PATH_TO_NEW}{coil}B{index}.csv", "w") as write_file:
 
                     ## Now we write the data to new csv and delete the old file, this
                     ## will lead to us having all cleaned and correct files in the
@@ -43,7 +47,7 @@ for coil in coil_df.coil:
                     write_file.write("Lengthpoints,Values\n")
                     write_file.writelines(lengthpoint + "," + value + "\n" for (lengthpoint, value) in clean_data)
 
-                    os.remove(f"../Metal_stuff/SignalExport/{coil}B{index}.csv")
+                    os.remove(f"{PATH_TO_OLD}/{coil}B{index}.csv")
 
             else:
                 bad_coils.append(coil)
@@ -53,15 +57,15 @@ for coil in coil_df.coil:
             bad_coils.append(coil)
             break
 
-final_coils = os.listdir("../Metal_stuff/coil_data")
+final_coils = os.listdir(PATH_TO_NEW)
 
 
 for coil in bad_coils:
     if f"{coil}B4.csv" in final_coils:
-        os.remove(f"../Metal_stuff/coil_data/{coil}B4.csv")
+        os.remove(f"{PATH_TO_NEW}{coil}B4.csv")
 
 coil_df = coil_df[coil_df.coil.isin(bad_coils) == False]
 
-coil_df.to_csv("../Metal_stuff/CoilData.csv")
+coil_df.to_csv(PATH_TO_CSV)
 
 print(f"Done! {len(bad_coils)} coils removed")
